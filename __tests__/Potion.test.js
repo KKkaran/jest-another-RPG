3aa49433dfdf2266f858f8b626252acdf6f2f903
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-12-14 23:50:15 -0500

    mock tests for potion done

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,9 +1,13 @@
 const Potion = require('../lib/Potion.js');
+//const Player = require()
 
-test('creates a random potion object', () => {
-    const potion = new Potion();
-  
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
-  });
+describe('creates a random potion object', () => {
+  describe("potion test",()=>{
+    it("potion testing",()=>{
+      const potion = new Potion();
+      expect(potion.name).toEqual(expect.any(String));
+      expect(potion.name.length).toBeGreaterThan(0);
+      expect(potion.value).toEqual(expect.any(Number));
+    })
+  })
+});
